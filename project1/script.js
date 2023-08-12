88dd8f404cb977cf8384904bd6ba90e4982f17c8
TistaChowdhury <116334037+TistaChowdhury@users.noreply.github.com>
2023-08-12 13:43:44 +0530

    Add files via upload

---
+++ project1/script.js
@@ -0,0 +1,17 @@
+{/* <script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const dropdownButton = document.getElementById("dropdown-btn");
+    const dropdownMenu = document.getElementById("dropdown-menu");
+
+    dropdownButton.addEventListener("click", function () {
+      dropdownMenu.classList.toggle("hidden");
+    });
+
+    // Close dropdown when clicking outside
+    document.addEventListener("click", function (event) {
+      if (!dropdownMenu.contains(event.target) && !dropdownButton.contains(event.target)) {
+        dropdownMenu.classList.add("hidden");
+      }
+    });
+  });
+</script> */}
